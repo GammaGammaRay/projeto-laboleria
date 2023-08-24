9dfe26d585f73ba0e43a4c040518ab287641a368
GammaGammaRay <benjamin.peret@gmail.com>
2023-08-23 22:22:52 -0300

    feat: app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { IndexRouter } from './routes/router';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(IndexRouter);
+
+const port = process.env.PORT || 5000
+app.listen(port,()=>{ console.log(`RUNNING IN PORT ${port}`)});
